ad74b30541a1634bfe4a1b49f31b4baa6052243e
Pardhu <pardhasaradhireddy00@gmail.com>
2026-06-09 18:13:58 +0530

    model evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.TextSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.TextSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.TextSummarizer.pipeline.stage_4_model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -25,15 +26,25 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-### Model downloaded from colab notebook due to GPU constraints
-# STAGE_NAME="Model Trainer stage"
+STAGE_NAME="Model Trainer stage"
 
-# try:
-#     logger.info(f"stage {STAGE_NAME} initiated")
-#     model_trainer_pipeline=ModelTrainerTrainingPipeline()
-#     model_trainer_pipeline.initiate_model_trainer()
-#     logger.info(f"Stage {STAGE_NAME} Completed")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
+STAGE_NAME = "Model Evaluation stage"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.TextSummarizer.constants import *
 from src.TextSummarizer.utils.common import read_yaml, create_directories
 
-from src.TextSummarizer.entity import DataIngestionConfig, DataTransformationConfig, ModelTrainerConfig
+from src.TextSummarizer.entity import DataIngestionConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -57,3 +57,17 @@ class ConfigurationManager:
             gradient_accumulation_steps = params.gradient_accumulation_steps
         )
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+        )
+        return model_evaluation_config
